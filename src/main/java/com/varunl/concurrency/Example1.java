cc633692441feb96d0baaac2f87b6fb7f29de27f
varunl <varunl@users.noreply.github.com>
2015-09-17 17:07:56 -0700

    Code showing race condition.

---
+++ src/main/java/com/varunl/concurrency/Example1.java
@@ -0,0 +1,38 @@
+package com.varunl.concurrency;
+
+
+/**
+ * This examaple shows that if 2 threads try to update a variable, then you will not have the correct output without
+ * using some sychronization techniques.
+ */
+public class Example1 {
+    private static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        // Create 2 threads who update the value of the counter.
+        Thread thread1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    count++;
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    count++;
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        // Wait for the threads to finish.
+        thread1.join();
+        thread2.join();
+
+        System.out.println("Expected value of count: 20000, Actual count: " + count);
+    }
+}
